31f1159cbe08dd77e11aed49bfe2b2c40efd7699
Chandreshpatle28 <patle269@gmail.com>
2024-11-13 20:09:30 +0530

    Added new file on 13 Nov

---
+++ src/test/java/handlingVariousWebElements/AutoSuggestiveDropdown.java
@@ -0,0 +1,47 @@
+package handlingVariousWebElements;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoSuggestiveDropdown {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");	
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		
+		WebElement autoSg = driver.findElement(By.xpath("//input[@id='autosuggest']"));
+		
+		autoSg.sendKeys("ger");
+		Thread.sleep(3000);
+//		
+//		autoSg.sendKeys(Keys.ARROW_DOWN);
+//		Thread.sleep(2000);
+//		
+//		autoSg.sendKeys(Keys.ARROW_DOWN);
+//		Thread.sleep(1000);
+//		
+//		autoSg.sendKeys(Keys.ENTER); //Select Germany As a country
+		
+		List<WebElement> cc = driver.findElements(By.xpath("//a[@class='ui-corner-all']"));
+		
+		for(int i=0; i<cc.size(); i++)
+		{
+			if(cc.get(i).getText().equals("Germany"))
+			{
+		        System.out.println("Your selected country name: " + cc.get(i).getText());
+				cc.get(i).click();
+				Thread.sleep(3000);
+			}
+		}
+		driver.close();
+	}
+
+}
